b87e0a1aca7e35ece9ed99d503d62944704db562
Uarlley do Nascimento Amorim <51249018+Uarlley@users.noreply.github.com>
2021-12-10 21:11:13 -0300

    So much progress

--- Utils/preprocessing.py
+++ Utils/preprocessing.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 
 def get_file_names(folder_path):
@@ -28,3 +29,28 @@ def fix_invalid_rows(dataset):
     return dataset.fillna(dataset.mean())
 
 
+def get_pressure_drop(X):
+    UP_Pessure = np.array(X['Upstream_Pressure(psi)'])
+    D_Pressure = np.array(X['Downstream_Pressure(psi)'])
+    return UP_Pessure-D_Pressure
+
+
+
+def get_labels(X):
+    pressure = get_pressure_drop(X)
+    pressure_threshold_index = np.where(pressure > 20)[0][0]
+
+    pressure_threshold_index
+    times = X['Time(s)']
+    threshold_time = times[pressure_threshold_index]
+
+    labels = [round(threshold_time - i, 1) if (threshold_time - i) > 0 else 0 for i in times]
+    return labels
+
+
+def prepare_dataset(X):
+
+    pressure = get_pressure_drop(X)
+    X = X.drop(['Flow_Rate(ml/m)', 'Upstream_Pressure(psi)','Downstream_Pressure(psi)'], axis=1)
+    X['pressure_drop'] = pressure
+    return X
